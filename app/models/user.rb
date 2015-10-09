c3079068e9470e20d77bd6b4259f028c54751516
Rodrigo Ra <rodrigorcomp@gmail.com>
2015-10-09 19:13:30 -0300

    setup login with facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  devise :omniauthable, :omniauth_providers => [:facebook]
+
+
+  class << self
+
+    def from_omniauth(auth)
+      byebug
+      where(provider: auth.provider, uid: auth.uid).first_or_create! do |user|
+        user.email = auth.info.email
+        user.email = "omniauth-#{auth.provider}-#{auth.uid}@facebook.com" unless user.email
+        user.password = Devise.friendly_token[0,20]
+      end
+    end
+  end
+
 end
